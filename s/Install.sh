123ee20fc8d0fde4c03465353728f19c99fc0c12
xijisz <qq75735889@gmail.com>
2024-10-27 21:01:46 +0800

    1

--- s/Install.sh
+++ s/Install.sh
@@ -2,8 +2,8 @@
 systemctl stop firewalld.service
 systemctl disable firewalld.service
 
-mkdir /s
-cd /s
+mkdir /root/s
+cd /root/s
 wget -O rathole https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathole
 wget -O s.toml https://github.com/trg58518/rathole/raw/refs/heads/main/s/s.toml
 wget -O /etc/systemd/system/rathole.service https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathole.service
