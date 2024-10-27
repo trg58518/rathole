ae94de6289943b7bb11540a0f75ba0e0a4ccafe1
xijisz <qq75735889@gmail.com>
2024-10-27 21:18:23 +0800

    1

--- s/Install.sh
+++ s/Install.sh
@@ -8,6 +8,7 @@ wget -O rathole https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathol
 wget -O s.toml https://github.com/trg58518/rathole/raw/refs/heads/main/s/s.toml
 wget -O /etc/systemd/system/rathole.service https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathole.service
 chmod -R 777 /root/s/
+chmod 777 /etc/systemd/system/rathole.service
 
 sudo systemctl start rathole
 sudo systemctl enable rathole
