7bab6969c0c4b8582470cfce9f504c1dbbf6b51e
xijisz <qq75735889@gmail.com>
2024-10-27 20:55:24 +0800

    1

--- s/Install.sh
+++ s/Install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir /s
 cd /s
-wget -O rathole https://github.com/trg58518/MTP/raw/main/gost
-wget -O s.toml https://github.com/trg58518/MTP/raw/main/gost
-wget -O /etc/systemd/system/rathole.service https://github.com/trg58518/MTP/raw/main/gost
+wget -O rathole https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathole
+wget -O s.toml https://github.com/trg58518/rathole/raw/refs/heads/main/s/s.toml
+wget -O /etc/systemd/system/rathole.service https://github.com/trg58518/rathole/raw/refs/heads/main/s/rathole.service
